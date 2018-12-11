7a541d007e1855b7bb9507a45415759c643e4523
wangfakun <449288317@qq.com>
2018-12-12 06:59:58 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/android/coolweather/com/coolweather/db/Province.java
@@ -0,0 +1,49 @@
+package android.coolweather.com.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by wangfakun on 2018/12/12.
+ */
+
+public class Province extends DataSupport {
+
+    private int id;
+
+    private String provinceName;
+
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Province{" +
+                "id=" + id +
+                ", provinceName='" + provinceName + '\'' +
+                ", provinceCode=" + provinceCode +
+                '}';
+    }
+}
